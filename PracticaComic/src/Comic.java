8c6ae3a69689878704f5f0faef5c9c0e7df2faca
RicAlc <94550398+RicAlc@users.noreply.github.com>
2022-04-29 12:54:36 -0500

    add: Basic version

--- PracticaComic/src/Comic.java
+++ PracticaComic/src/Comic.java
@@ -1,3 +1,5 @@
+import java.io.Closeable;
+
 public class Comic extends Thread{
     public static void main(String[] args) {
         Comic h1 = new Comic();
@@ -8,24 +10,28 @@ public class Comic extends Thread{
 
     @Override
     public void run() {
+
         Screen page  = new Screen();
-        Screen page2 = new Screen();
-        page.setBounds(660, 0, 600, 1000);
+        page.setBounds(660, 0, 640, 1010);
         page.setVisible(true);
         for (int i = 1; i <=12; i++){
             try{
+                if (i >2){
+                    page.setBounds(660, 0, 600, 810);
+                }
                 page.showImage("comic"+ i + ".JPG");
+                System.out.println(i);
 
 
-//                System.out.println(i);
-                Thread.sleep(500);
-
-
+                Thread.sleep(10000);
+                if (i != 12){
+                    page.cls();
+                }
             }catch (InterruptedException ie){
                 ie.printStackTrace();
             }
         }
-
+        Thread.interrupted();
     }
 
 }
